dbf8f5ff7f8aa71a3b7a58052ffd3f71a25667ef
Khawaja Farooq <kfarooqa@gmail.com>
2017-12-14 22:01:09 +0300

    Initial commit

---
+++ app/src/main/java/com/xmen/miwok/MainActivity.java
@@ -0,0 +1,20 @@
+package com.xmen.miwok;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openNumbersList(View v) {
+        Intent intent = new Intent(this, NumbersActivity.class);
+        startActivity(intent);
+    }
+}
