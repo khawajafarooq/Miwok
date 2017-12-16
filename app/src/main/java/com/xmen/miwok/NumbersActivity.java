4350094eb1276773ef815160816f48e51f4dffad
Khawaja Farooq <kfarooqa@gmail.com>
2017-12-16 11:31:23 +0300

    adding miwok translation for numbers

--- app/src/main/java/com/xmen/miwok/NumbersActivity.java
+++ app/src/main/java/com/xmen/miwok/NumbersActivity.java
@@ -22,15 +22,15 @@ public class NumbersActivity extends AppCompatActivity {
 
 
         words.add(new Word("one", "lutti"));
-        words.add(new Word("two", "lutti"));
-        words.add(new Word("three", "lutti"));
-        words.add(new Word("four", "lutti"));
-        words.add(new Word("five", "lutti"));
-        words.add(new Word("six", "lutti"));
-        words.add(new Word("seven", "lutti"));
-        words.add(new Word("eight", "lutti"));
-        words.add(new Word("nine", "lutti"));
-        words.add(new Word("ten", "lutti"));
+        words.add(new Word("two", "otiiko"));
+        words.add(new Word("three", "tolookuso"));
+        words.add(new Word("four", "oyyisa"));
+        words.add(new Word("five", "massokka"));
+        words.add(new Word("six", "temmokka"));
+        words.add(new Word("seven", "kenekaku"));
+        words.add(new Word("eight", "kawinta"));
+        words.add(new Word("nine", "wo'e"));
+        words.add(new Word("ten", "na'aacha"));
 
         WordAdapter adapter = new WordAdapter(this, words);
 
