54fd497539447b585facac5156bb3dd7e57e16e3
Khawaja Farooq <kfarooqa@gmail.com>
2017-12-16 13:43:43 +0300

    visual appearace improvement

--- app/src/main/java/com/xmen/miwok/Word.java
+++ app/src/main/java/com/xmen/miwok/Word.java
@@ -4,11 +4,20 @@ public class Word {
 
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int mImageResourceId;
 
+    private static final int NO_IMAGE_PROVIDED = -1;
 
     public Word(String defaultTranslation, String miwokTranslation) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+        mImageResourceId = NO_IMAGE_PROVIDED;
+    }
+
+    public Word(String defaultTranslation, String miwokTranslation, int ImageResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = ImageResourceId;
     }
 
     public String getDefaultTranslation() {
@@ -17,5 +26,11 @@ public class Word {
 
     public String getMiwokTranslation() {
         return mMiwokTranslation;
+    }
+
+    public int getImageResourceId() { return  mImageResourceId; }
+
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
     }
 }
--- app/src/main/java/com/xmen/miwok/WordAdapter.java
+++ app/src/main/java/com/xmen/miwok/WordAdapter.java
@@ -3,42 +3,66 @@ package com.xmen.miwok;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
-    public WordAdapter(Context context, ArrayList<Word> words) {
+    private int mBackgroundColor;
+
+    public WordAdapter(Context context, ArrayList<Word> words, int backgroundColor) {
         super(context, R.layout.list_item, words);
+        mBackgroundColor = backgroundColor;
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
-        // Get the data
-        Word word = getItem(position);
+        View listView = convertView;
 
         // Checking if existing view is being reused
-        if (convertView == null) {
+        if (listView == null) {
 
             // inflat one yourself
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, null);
+            listView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, null);
         }
 
-        // set the data
+        // set the color
+        int color = ContextCompat.getColor(getContext(), mBackgroundColor);
+        LinearLayout textContainer = listView.findViewById(R.id.list_item_color);
+        textContainer.setBackgroundColor(color);
 
-        TextView defaultTextView = convertView.findViewById(R.id.default_text_view);
-        TextView miwokTextView = convertView.findViewById(R.id.miwok_text_view);
+        // Get the data
+        Word currentWord = getItem(position);
 
-        defaultTextView.setText(word.getDefaultTranslation());
-        miwokTextView.setText(word.getMiwokTranslation());
+        // Set the data
+        TextView defaultTextView = listView.findViewById(R.id.default_text_view);
+        TextView miwokTextView = listView.findViewById(R.id.miwok_text_view);
 
-        return convertView;
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        ImageView image = listView.findViewById(R.id.image);
+
+        if (currentWord.hasImage()) {
+            image.setImageResource(currentWord.getImageResourceId());
+
+            // For view reusability
+            image.setVisibility(View.VISIBLE);
+        } else {
+            image.setVisibility(View.GONE);
+        }
+
+        return listView;
     }
 }
